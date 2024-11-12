7af85adca9f905ba2f33a0ecf188f0f52b0fd4d9
NPT0116 <thanh1612004@gmail.com>
2024-11-12 16:19:29 +0700

    fix bcryp

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,5 +1,5 @@
 import { User } from '../models/userSchema.mjs';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export const createUser = async (username, password, email) => {
     try {
